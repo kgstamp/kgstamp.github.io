8ae22cd56850bff3dc316bfbee83b7b95e3d0b28
kgstamp <kgstamp@umich.edu>
2025-08-20 16:01:31 -0400

    added some files and structure

---
+++ index.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+export default function CurrentPage() {
+    return (
+        <div>
+            <body>
+                <p>Hello world.</p>
+            </body>
+        </div>
+    );
+}
+
+// ok maybe not doing react, just regular js. so ignore all this for now
